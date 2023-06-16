bccaf25e101881e1e803c0739c05675c81aef389
mokhtarTarek <tmkh@outlook.fr>
2023-06-16 14:38:47 +0100

    first commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { IoMdAdd, IoMdSearch } from 'react-icons/io';
 
-const NavBar = () => {
-    return (
-        <div>NavBar</div>
-    )
-}
+const Navbar = ({ searchTerm, setSearchTerm, user }) => {
+    // this component accept the searchTerm and it's calback setter from the parent component and mnipulate them
+    const navigate = useNavigate();
 
-export default NavBar
+    if (user) {
+        return (
+            <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7 ">
+                <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
+                    <IoMdSearch fontSize={21} className="ml-1" />
+                    <input
+                        type="text"
+                        // e.target is the node where the even hapend
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        placeholder="Search"
+                        value={searchTerm}
+                        // Changing the url using useNavigate() will cause
+                        // the Router component that contain this path to render the relative element
+                        onFocus={() => navigate('/search')}
+                        className="p-2 w-full bg-white outline-none"
+                    />
+                </div>
+                <div className="flex gap-3 ">
+                    <Link to={`user-profile/${user?._id}`} className="hidden md:block">
+                        <img src={user.image} alt="user-pic" className="w-14 h-12 rounded-lg " />
+                    </Link>
+                    <Link to="/create-pin" className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center">
+                        <IoMdAdd />
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+
+    return null;
+};
+
+export default Navbar;
--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,6 +1,6 @@
+// THIS IS CONTAINER COMPONENTS
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components';
 
 const Pins = ({ user }) => {
